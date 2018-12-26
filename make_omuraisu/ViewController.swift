794525c52f32c49cdf5de6490d19d0ff58774737
Yuto Nakano <yuto.44.bb@gmail.com>
2018-12-26 14:36:56 +0900

    Initial Commit

---
+++ make_omuraisu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  make_omuraisu
+//
+//  Created by 中野湧仁 on 2018/12/26.
+//  Copyright © 2018年 中野湧仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
